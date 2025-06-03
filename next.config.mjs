84fad7139868125db01ab801e4604be0a5c259c6
isaaclins <isaac.lins07@gmail.com>
2025-06-03 14:13:28 +0200

    chore: project initialisation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 import path from 'path';
 /** @type {import('next').NextConfig} */
+
+// Determine if the app is running on GitHub Pages
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // Replace '<repository-name>' with your repository name
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
-    output: 'standalone',
+    output: 'export',
+    // Optional: Add a trailing slash to all URLs.
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: {
+        unoptimized: true,
+    },
 };
 export default nextConfig;
